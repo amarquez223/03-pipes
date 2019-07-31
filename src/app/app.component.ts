c975927c386fb81e3d6931e27a791fdd0361277b
Anderson Marquez <anderson.marquez@gmail.com>
2019-07-31 11:10:46 -0500

    Pipes en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre = "Ánderson";
+
+  nombre2 = "ánderSOn márquez ríOS";
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  };
+
+  valorDePromesa = new Promise((resolve, reject) => {
+
+    setTimeout(() => resolve('Llegaron los datos!!'), 3500);
+
+  });
+
+  fecha = new Date();
+
+  video = "k5E2AVpwsko";
+
+  activar: boolean = true;
+
 }
